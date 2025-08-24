b0912e5fbfae522adf49bbe99a2d1861ebb9c8a6
felixpanzo150-pr <felixpanzo150@gmail.com>
2025-08-24 22:19:41 +0100

    Feat: primeiro commit

---
+++ src/main/java/tech/felixpanzo/megalums/MegalumsApplication.java
@@ -0,0 +1,13 @@
+package tech.felixpanzo.megalums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegalumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegalumsApplication.class, args);
+	}
+
+}
